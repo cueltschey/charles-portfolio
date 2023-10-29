aae59d709364159beba9fe0bb9c35b8f6af8e8e8
cueltschey <chaseuelt@gmail.com>
2023-10-28 21:03:23 -0500

    added home page

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,20 @@
+import "./Home.css"
+
+
+const Home = () => {
+    
+    return(
+        <div className="home">
+           <div className="description">
+                <h1>Hi there, I'm Charles</h1>
+                <p>I'm a junior developer who is working hard to impove my
+                skills and master the art of programming. I'm currently enrolled
+                at Mississippi State University as a sophomore. I'm passionate
+                about software development and programming</p>
+           </div> 
+           <div className="picture"></div> 
+        </div>
+    )
+}
+
+export default Home
