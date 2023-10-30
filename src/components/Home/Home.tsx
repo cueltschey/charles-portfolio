6e34a87b5fcab4acaa221eeadb3d01e645993ed1
cueltschey <chaseuelt@gmail.com>
2023-10-29 22:25:00 -0500

    starting to make home animation

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,13 @@
 import "./Home.css"
-
-
-const Home = () => {
-    
+import description from 'description.txt';
+const Home = () => {    
     return(
         <div className="home">
            <div className="description">
                 <h1>Hi there, I'm Charles</h1>
-                <p>I'm a junior developer who is working hard to impove my
-                skills and master the art of programming. I'm currently enrolled
-                at Mississippi State University as a sophomore. I'm passionate
-                about software development and programming</p>
+                {description.map((word : string, index : number) => (
+                    <h5 className="fadeIn">{word}</h5>    
+                ))}
            </div> 
            <div className="picture"></div> 
         </div>
