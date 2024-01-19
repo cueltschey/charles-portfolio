afa9d3316a19cb5068ff1e63514ad65191abe50d
cueltschey <chaseuelt@gmail.com>
2024-01-18 18:39:44 -0600

    fixing bugs

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/', (req, res) => {
   // Handle requests for the root route
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist'));
 });
 
 app.listen(port, () => {
